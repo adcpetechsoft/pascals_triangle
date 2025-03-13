5c9b793a745f0a24384d44b9412bbfdfd6a8a585
amd <adcpetechsoft@hotmail.com>
2025-03-13 17:03:14 +0800

    final

--- pascals_triangle.cpp
+++ pascals_triangle.cpp
@@ -1,32 +1,54 @@
+/*
+    Pascal's Triangle Progrma
+    As a submission to https://code.golf/pascals-triangle#cpp
+
+    Created by: amd
+*/
+
+// ---------------------------------
+// Preprocessor Declaration
+
 #define MAX_NUM 20
 
 #include <iostream>
-#include <string>
 
+// ---------------------------------
+// Progran  Entry Point
 int main(int argc, char *argv[])
 {
+    
+    // ***********************
+    // Variable Declaration
     using namespace std;
 
     int i;
     int j;
-    int k;
 
     int num[MAX_NUM][MAX_NUM];
 
+    // ***********************
+    // Actual Procedure
+
+    // looping for display horizontal
     i=0;
     while(i<MAX_NUM)
     {
+        // crating and placing values to variables 
+
+        // for 1st display value
         if(i==0)
         {
             num[i][0]=1;
         };
 
+        // for 2nd display value
         if(i==1)
         {
             num[i][0]=1;
             num[i][1]=1;
         };
 
+        // suceeding display value
         if(i>=1)
         {
             num[i][0]=1;
@@ -38,9 +60,11 @@ int main(int argc, char *argv[])
                 j++; 
             };
 
+            // for last display value
             num[i][j]=1;
         };
 
+        // looping for vertical print to screen
         j=0;
         while(j<=i)
         {
@@ -52,7 +76,7 @@ int main(int argc, char *argv[])
 
         i++;
     };
-    
-    return 0;
 
+    return 0;
+    
 };
